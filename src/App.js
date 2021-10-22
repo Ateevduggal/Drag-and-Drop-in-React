f6927fb7e3245a8392a26db6623ce53cbbf6aa62
AteevDuggal <akd3257@gmail.com>
2021-10-22 16:43:04 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
 
-function App() {
+const App = () => {
+  const drop = (e) => {
+    e.preventDefault();
+    if(e.dataTransfer.getData("user")) {
+      const card_id = e.dataTransfer.getData("user");
+
+      const card = document.getElementById(card_id);
+      card.style.display = "block";
+
+      e.target.appendChild(card);
+    }
+  };
+
+  const dragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const dragStart = (e) => {
+    const target = e.target;
+    e.dataTransfer.setData("user", target.id);
+    setTimeout(() => {
+      target.style.display = "none";
+    }, 0);
+  };
+
+  const dragOverEven = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 fs-1 fw-bold text-center">
+            Drag And Drop Tutorial
+          </div>
+          <div className="col-12 d-flex justify-content-center mt-5">
+            <div className="mx-4 text-center">
+              <h1>Team 1</h1>
+              <div
+                className="d-flex flex-column bg-info bga align-items-center"
+                onDrop={drop}
+                onDragOver={dragOver}
+              >
+                {data.map((Val, id) => {
+                  return (
+                    <button
+                      onDragStart={dragStart}
+                      onDragOver={dragOverEven}
+                      draggable="true"
+                    >
+                      {Val.name}
+                      <div>{Val.color}</div>
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="mx-4 tetx-center">
+              <h1>Team 2</h1>
+              <div
+                className="d-flex flex-column bg-warning bga"
+                onDrop={drop}
+                onDragOver={dragOver}
+              > </div>
+            </div>
+            <div className="mx-4 tetx-center">
+              <h1>Team 3</h1>
+              <div
+                className="d-flex flex-column bg-warning bga"
+                onDrop={drop}
+                onDragOver={dragOver}
+              > </div>
+            </div>
+            <div className="mx-4 tetx-center">
+              <h1>Team 4</h1>
+              <div
+                className="d-flex flex-column bg-warning bga"
+                onDrop={drop}
+                onDragOver={dragOver}
+              > </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data.jsx
@@ -0,0 +1,76 @@
+export const data =  [
+    {
+      id:'1',
+      name: "player1",
+      color: "#E8736F",
+      team: "notAssigned",
+    },
+    {
+      id:'2',
+      name: "player2",
+      color: "#45C79B",
+      team: "notAssigned",
+    },
+    {
+      id:'3',
+      name: "player3",
+      color: "E8736F",
+      team: "notAssigned",
+    },
+    {
+      id:'4',
+      name: "player4",
+      color: "#FFBE17",
+      team: "notAssigned",
+    },
+    {
+      id:'5',
+      name: "player5",
+      color: "#6A75CA",
+      team: "notAssigned",
+    },
+    {
+      id:'6',
+      name: "player6",
+      color: "#E8736F",
+      team: "notAssigned",
+    },
+    {
+      id:'7',
+      name: "player7",
+      color: "E8736F",
+      team: "notAssigned",
+    },
+    {
+      id:'8',
+      name: "player8",
+      color: "#6A75CA",
+      team: "notAssigned",
+    },
+    {
+      id:'9',
+      name: "player9",
+      color: "E8736F",
+      team: "notAssigned",
+    },
+    {
+      id:'10',
+      name: "player10",
+      color: "#FFBE17",
+      team: "notAssigned",
+    },
+    {
+      id:'11',
+      name: "player11",
+      color: "#FFBE17",
+      team: "notAssigned",
+    },
+    {
+      id:'12',
+      name: "player12",
+      color: "#FFBE17",
+      team: "notAssigned",
+    },
+  ];
+
+  export default data;
